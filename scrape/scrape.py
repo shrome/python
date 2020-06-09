164aa9eab619042468412e829d3003446ea5e0ac
shrome <46410167+shrome@users.noreply.github.com>
2020-06-09 14:44:39 +0800

    Update scrape.py
    make a little change

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -5,7 +5,7 @@ from requests import Session
 
 session = requests.Session()
 per_session = session.post("https://www.exam-mate.com/reguser/checklogin", 
-data={'email':'lawliet5145@gmail.com', 'password':'kAmehAmehA1!'})
+data={'email':'lawliet5145@gmail.com', 'password':'kAmehAmehA1!'},timeout=None)
 
 NewArr=[]
 arr=[]
@@ -22,40 +22,43 @@ for s in range(0,10,20):
 CountStr = 0
 counting = 0
 count = 0
-while (count < len(arrUrl)):
-#    response = urllib.request.urlopen(arrUrl[count])
-    content = BeautifulSoup(session.get(arrUrl[count]).content , "html.parser")
-    if (content.find_all('a', class_="qabtn") == None):
-        break
-    else:
-        for anc in content.find_all('a', class_="qabtn"):
-            splitting = anc['onclick'].split(",")[1]
-            spli = splitting.translate({ord("'"): None})
-            if len(spli) > 2:
-                adding = "https://www.exam-mate.com"+spli
-                spli = adding.replace(" ","")
-            arr.append(spli)
+try:
+    while (count < len(arrUrl)):
+    #    response = urllib.request.urlopen(arrUrl[count])
+        content = BeautifulSoup(session.get(arrUrl[count]).content , "html.parser")
+        if (content.find_all('a', class_="qabtn") == None):
+            break
+        else:
+            for anc in content.find_all('a', class_="qabtn"):
+                splitting = anc['onclick'].split(",")[1]
+                spli = splitting.translate({ord("'"): None})
+                if len(spli) > 2:
+                    adding = "https://www.exam-mate.com"+spli
+                    spli = adding.replace(" ","")
+                arr.append(spli)
 
-        for tit in content.find_all("div" , class_="question"):
-            for title in tit.find_all("div"):
-                AlasOfLast.append(title.string)
+            for tit in content.find_all("div" , class_="question"):
+                for title in tit.find_all("div"):
+                    AlasOfLast.append(title.string)
 
-        while (CountStr < len(AlasOfLast)):
-            if (AlasOfLast[CountStr] == None):
-                del AlasOfLast[CountStr]
-            CountStr += 1
+            while (CountStr < len(AlasOfLast)):
+                if (AlasOfLast[CountStr] == None):
+                    del AlasOfLast[CountStr]
+                CountStr += 1
 
-        while (counting < len(AlasOfLast)):
-            if (counting % 2 == 0):
-                lenOfArr.append(AlasOfLast[counting])
-            counting += 1
-            
-        count += 1
+            while (counting < len(AlasOfLast)):
+                if (counting % 2 == 0):
+                    lenOfArr.append(AlasOfLast[counting])
+                counting += 1
+
+            count += 1
 
 
-for n in range(len(lenOfArr)) :
-    NewArr.append(lenOfArr[n].strip())
-
+    for n in range(len(lenOfArr)) :
+        NewArr.append(lenOfArr[n].strip())
+except:
+    print("timeout or connection reset by peer")
+    
 ques = []
 answ = []
 
@@ -67,28 +70,30 @@ for j in range(len(arr)):
 
 quest = []
 answe = []
-for i in range(len(ques)):
-    for k in range(1,11):
-        cont = ques[i].replace('_1.png',"_{k}.png".format(k=k)) if (ques[i].endswith("png")) else ques[i].replace('_1.jpg',"_{k}.jpg".format(k=k)) if (ques[i].endswith("jpg")) else ques[i].replace('_1.jpeg',"_{k}.jpeg".format(k=k))
-        if (requests.get(cont).status_code == 200):
-            quest.append(cont)
-            print(cont)
-        cant = answ[i].replace('_1.png',"_{k}.png".format(k=k)) if (answ[i].endswith("png")) else answ[i].replace('_1.jpg',"_{k}.jpg".format(k=k)) if (answ[i].endswith("jpg")) else answ[i].replace('_1.jpeg',"_{k}.jpeg".format(k=k)) if (answ[i].endswith('jpeg')) else answ[i]
-        try:
-            if (answ[i].endswith("png") or answ[i].endswith("jpg") or answ[i].endswith("jpeg")):
-                if (requests.get(cant).status_code == 200):
+try:
+    for i in range(len(ques)):
+        for k in range(1,11):
+            cont = ques[i].replace('_1.png',"_{k}.png".format(k=k)) if (ques[i].endswith("png")) else ques[i].replace('_1.jpg',"_{k}.jpg".format(k=k)) if (ques[i].endswith("jpg")) else ques[i].replace('_1.jpeg',"_{k}.jpeg".format(k=k))
+            if (requests.get(cont).status_code == 200):
+                quest.append(cont)
+                print(cont)
+            cant = answ[i].replace('_1.png',"_{k}.png".format(k=k)) if (answ[i].endswith("png")) else answ[i].replace('_1.jpg',"_{k}.jpg".format(k=k)) if (answ[i].endswith("jpg")) else answ[i].replace('_1.jpeg',"_{k}.jpeg".format(k=k)) if (answ[i].endswith('jpeg')) else answ[i]
+            try:
+                if (answ[i].endswith("png") or answ[i].endswith("jpg") or answ[i].endswith("jpeg")):
+                    if (requests.get(cant).status_code == 200):
+                        answe.append(cant)
+                        print(cant)
+                    else:
+                        pass
+                        break
+                else:
                     answe.append(cant)
                     print(cant)
-                else:
-                    pass
                     break
-            else:
-                answe.append(cant)
-                print(cant)
-                break
-        except:
-            print("not image error")
-        
+            except:
+                print("not image error")
+except:
+    print("connection reset by peer or time out")
 
 f = open("hello.js", "w")
 f.write("var ques = {};\n".format(quest))
